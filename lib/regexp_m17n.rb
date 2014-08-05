b875f03e0f939a5e0fda5a62d4ba0930727e47ba
Michael Nikitochkin <miry.sof@gmail.com>
2014-08-05 11:56:11 +0300

    Wip

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,24 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ /^.+$/
+    str_encoding = str.encoding
+    p str_encoding
+    p str_encoding.ascii_compatible?
+    p str_encoding.dummy?
+
+    # Returns true for dummy encodings. A dummy encoding is an encoding
+    # for which character handling is not properly implemented. It is
+    # used for stateful encodings.
+    pattern = '^.+$'
+    enc_pattern = pattern
+
+    unless str_encoding.dummy?
+      enc_pattern = pattern.encode(str_encoding)
+    end
+
+    str =~ Regexp.new(enc_pattern)
+  rescue => e
+    puts "Error"
+    puts e
+    puts e.backtrace.first
   end
 end
--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -5,7 +5,8 @@ require_relative '../lib/regexp_m17n'
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
     Encoding.list.each do |enc|
-      assert(RegexpM17N.non_empty?('.'.encode(enc)))
+      text_encoded = '.'.encode(enc)
+      assert(RegexpM17N.non_empty?(text_encoded))
     end
   end
 end
